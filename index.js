3b50b401cd16a4fa32173695035b4db43b0ab6ac
Artem  F <artemfm@gmail.com>
2021-02-03 16:53:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // add solution here
+
+function theBeatlesPlay(musicians, instruments) {
+  var array = [];
+for(var n = 0; n < musicians.length; n++) {
+array.push(`${musicians[n]} plays ${instruments[n]}`);
+}
+return array;
+}
+
+function johnLennonFacts(array) {
+  var n = ["He hated the sound of his own voice"]
+  while (n < array.length) {
+    array[n] = array[n] + "!!!";
+    n++;
+  }
+  return array;}
+  
+function johnLennonFacts(array) {
+var n = ["He hated the sound of his own voice", "He was never a vegetarian" ];
+while (n < array.length) {
+array[n] = array[n] + "!!!";
+ n++;
+}
+return array;}
