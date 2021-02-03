7b2cc9dae3e29e4747e1a18132fe1f135ee8a3d5
Artem  F <artemfm@gmail.com>
2021-02-03 16:58:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ return array;
 
   
 function johnLennonFacts(array) {
-var n = [0];
-while (n < array.length) {
+  var n = [0];
+  while (n < array.length) {
 array[n] = array[n] + "!!!";
  n++;
 }
